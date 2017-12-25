5132c5f744e5e16c1e9ff8f7fb0e97d4ffbe4ef4
Patrick McCarty <patrickthesailorman@gmail.com>
2017-12-25 19:17:37 +0000

    Remove old math, Add operatorInput to clear

--- script.js
+++ script.js
@@ -1,97 +1,46 @@
 
-
-    var calculator = {
-        display: [],
-        numbers: [],
-        add: function(num1, num2) {
-            var sum = num1 + num2;
-            this.display.value = sum;
-            document.getElementById("display").innerHTML = sum;
-        },
-        subtract: function(num1, num2) {
-            var sum = num1 - num2;
-            document.getElementById("display").innerHTML = sum;
-        },
-        multiply: function(num1, num2) {
-            var sum = num1 * num2;
-            document.getElementById("display").innerHTML = sum;
-        },
-        divide: function(num1, num2) {
-            var sum = num1 / num2;
-            document.getElementById("display").innerHTML = sum;
-        },
-    };
-
-    
-var num1 = [];
-var num2 = [];
 var handlers = {
     add: function() {
         var operatorInput = document.getElementById('operatorInput');
-        document.getElementById('display');
-        num1.push(parseInt(calculator.display.value));
-        calculator.add(parseInt(calculator.display.value));
         operatorInput.value = '+';
-        view.display();
+       
   },
 
     subtract: function() {
         var operatorInput = document.getElementById('operatorInput');
-        num1.push(parseInt(calculator.display.value));
-        calculator.subtract(parseInt(calculator.display.value));
         operatorInput.value = '-';
-        view.calculator.display();
+        
   },
   
     multiply: function() {
         var operatorInput = document.getElementById('operatorInput');
-        num1.push(parseInt(calculator.display.value));
-        calculator.multiply(parseInt(calculator.display.value));
         operatorInput.value = 'x';
-        view.calculator.display();
+        
   },
   
     divide: function() {
         var operatorInput = document.getElementById('operatorInput');
-        num1.push(parseInt(calculator.display.value));
-        calculator.divide(parseInt(calculator.display.value));
         operatorInput.value = '/';
-        view.calculator.display();
+        
   },
   
-    equals: function() {
-        var operatorInput = document.getElementById('operatorInput');
-        num2.push(parseInt(calculator.display.value));
-        if(operatorInput.value === '+') {
-            calculator.add(num1, num2);
-        } else if(operatorInput.value === '-') {
-            calculator.subtract(num1, num2);
-        } else if(operatorInput.value === 'x') {
-            calculator.multiply(num1, num2);
-        } else if(operatorInput.value === '/') {
-            calculator.divide(num1, num2);
-        } else {
-            calculator.display.value;
-        }
-        operatorInput.value = '=';
-        view.display();
-  },
   clearDisplay: function() {
             var display = document.getElementById('display');
             display.value = '0';
             storedNum = '0';
             calculationFinished = true;
             operation = operations.none;
+            operatorInput.value = '';
         } 
   
 };
  var operations = {
-    // no-op. Takes the right side, and just returns it.  Since the right side is the
-    // display value, and calculate() sets display.value, this effectively makes
-    // calculate() say "display.value = +display.value".
+    /* no-op. Takes the right side, and just returns it.  Since the right side is the
+    display value, and calculate() sets display.value, this effectively makes
+    calculate() say "display.value = +display.value".*/
     none:     function(left, right) { return right; },
 
-    // Math ops.
+    // Math operations.
     add:      function(left, right) { return left + right; },
     subtract: function(left, right) { return left - right; },
     multiply: function(left, right) { return left * right; },
