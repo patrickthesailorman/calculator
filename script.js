25ca062a7755bf71bf36c1fe6dbc11fd4bb3f134
Patrick McCarty <patrickthesailorman@gmail.com>
2017-12-25 21:55:28 +0000

    Add set operation and remove old code

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var display;
 var calculationFinished;
 var operation;
 var storedNum;
+
 var handlers = {
     add: function() {
         operatorInput = document.getElementById('operatorInput');
@@ -75,93 +76,4 @@ function calculate() {
     operation = operations.none;
 }
 
-    // document.getElementById('zero').onclick = function() {
-
-    //     calculator.display.push(0);
-    //     console.log(calculator.display);
-    //     document.getElementById('display').value = calculator.display.join("");
-    // },
-
-    // document.getElementById('value1').onclick = function() {
-
-    //     calculator.display.push(1);
-    //     console.log(calculator.display);
-    //     document.getElementById('display').value = calculator.display.join("");
-    // },
-    
-    // document.getElementById('value2').onclick = function() {
-
-    //     calculator.display.push(2);
-    //     console.log(calculator.display);
-    //     document.getElementById('display').value = calculator.display.join("");
-    // },
-    
-    // document.getElementById('value3').onclick = function() {
-
-    //     calculator.display.push(3);
-    //     console.log(calculator.display);
-    //     document.getElementById('display').value = calculator.display.join("");
-    // },
-    
-    // document.getElementById('value4').onclick = function() {
-
-    //     calculator.display.push(4);
-    //     console.log(calculator.display);
-    //     document.getElementById('display').value = calculator.display.join("");
-    // },
-    
-    // document.getElementById('value5').onclick = function() {
-
-    //     calculator.display.push(5);
-    //     console.log(calculator.display);
-    //     document.getElementById('display').value = calculator.display.join("");
-    // },
-    
-    // document.getElementById('value6').onclick = function() {
-
-    //     calculator.display.push(6);
-    //     console.log(calculator.display);
-    //     document.getElementById('display').value = calculator.display.join("");
-    // },
-    
-    // document.getElementById('value7').onclick = function() {
-
-    //     calculator.display.push(7);
-    //     console.log(calculator.display);
-    //     document.getElementById('display').value = calculator.display.join("");
-    // },
-    
-    // document.getElementById('value8').onclick = function() {
-
-    //     calculator.display.push(8);
-    //     console.log(calculator.display);
-    //     document.getElementById('display').value = calculator.display.join("");
-    // },
-     
-    // document.getElementById('value9').onclick = function() {
-
-    //     calculator.display.push(9);
-    //     console.log(calculator.display);
-    //     document.getElementById('display').value = calculator.display.join("");
-    // };
-
-    
-// var view = {
-//   display: function() {
-//     var display = document.querySelector('display');
-//     calculator.display.value = '';
-//     var operatorInput = document.getElementById('operatorInput');
-//     calculator.display.forEach(function(num, position) {
-//       var num1 = [];
-//       var num2 = [];
-
-//       if (operatorInput === true) {
-//         num2.push(calculator.display.value);
-//       } else {
-//         num1.push(calculator.display.value);
-//       }
-  
-//     }, this);
-// }
-// }
 
