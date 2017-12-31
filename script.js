c94c50601df999b6eac30f09c1bf4751e5697b57
Patrick McCarty <patrickthesailorman@gmail.com>
2017-12-31 16:15:15 +0000

    change / to division symbol

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ var handlers = {
   
     divide: function() {
         operatorInput = document.getElementById('operatorInput');
-        operatorInput.value = '/';
+        // operatorInput.value = '&#247';
     },
   
     equals: function() {
