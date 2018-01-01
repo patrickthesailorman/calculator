835c7c5c9f3dbb9f2281d929337826e8223a9156
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-01 03:45:16 +0000

    Add division symbol

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ var handlers = {
   
     divide: function() {
         operatorInput = document.getElementById('operatorInput');
-        // operatorInput.value = '&#247';
+
     },
   
     equals: function() {
